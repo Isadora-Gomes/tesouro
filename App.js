5395c7ed7e2a4e3e7785c468795e3b7966d14e88
Gabriel-Britoo <gabrito343@gmail.com>
2025-09-18 14:13:24 -0300

    expo-location

--- App.js
+++ App.js
@@ -1,11 +1,49 @@
-import { StatusBar } from 'expo-status-bar';
+import * as Location from 'expo-location';
+import { useState, useEffect } from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 
 export default function App() {
+  const [location, setLocation] = useState(null);
+  const [errorMsg, setErrorMsg] = useState(null);
+
+  useEffect(() => {
+    let subscription;
+
+    (async () => {
+      let { status } = await Location.requestForegroundPermissionsAsync();
+      if (status !== 'granted') {
+        setErrorMsg('Permissão para acessar localização negada');
+        return;
+      }
+
+      subscription = await Location.watchPositionAsync(
+        {
+          accuracy: Location.Accuracy.High,
+          timeInterval: 500,
+          distanceInterval: 0.5,
+        },
+        (loc) => {
+          setLocation(loc);
+        }
+      );
+    })();
+
+    return () => {
+      if (subscription) {
+        subscription.remove();
+      }
+    };
+  }, []);
+
   return (
     <View style={styles.container}>
-      <Text>Caça tesouro!</Text>
-      <StatusBar style="auto" />
+      <Text>
+        {errorMsg
+          ? errorMsg
+          : location
+          ? JSON.stringify(location.coords, null, 2)
+          : "Carregando localização..."}
+      </Text>
     </View>
   );
 }
