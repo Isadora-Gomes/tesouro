eea73129f9e2f68369280926f73f66f31815ee79
Isadora-Gomes <isasilvacpv@gmail.com>
2025-09-18 12:09:38 -0300

    inicuo

--- App.js
+++ App.js
@@ -4,7 +4,7 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text>Caça tesouro!</Text>
       <StatusBar style="auto" />
     </View>
   );
